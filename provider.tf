8f849d676d32a4ba1de86fd94c57245048504c5b
tcwong2024 <tcwong99@hotmail.com>
2024-09-22 00:32:54 +0800

    hashicorp/aws v4.67.0

--- provider.tf
+++ provider.tf
@@ -2,7 +2,8 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      #version = "~> 5.0"
+      version = "~> 4.66"  # Use a stable version, e.g., 4.66.x
     }
   }
   required_version = ">= 1.2.0"
