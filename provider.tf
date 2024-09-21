01bfa414abe4359239755af88a52113f108c2633
tcwong2024 <tcwong99@hotmail.com>
2024-09-22 00:09:03 +0800

    update tf 5.25.0

--- provider.tf
+++ provider.tf
@@ -1,10 +1,11 @@
 terraform {
-  required_providers {
+    required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.68.0"
+      version = "5.25.0"
     }
   }
+  required_version = ">= 1.2.0"
 }
 
 provider "aws" {
