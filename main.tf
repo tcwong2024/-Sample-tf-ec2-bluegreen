fe9e0cf3b89dca9ec599a4b10fb10c92239b03f3
tcwong2024 <tcwong99@hotmail.com>
2024-09-23 20:28:31 +0800

    enable  green

--- main.tf
+++ main.tf
@@ -85,7 +85,8 @@ resource "aws_lb_listener" "app" {
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.blue.arn
+    #target_group_arn = aws_lb_target_group.blue.arn
+    target_group_arn = aws_lb_target_group.green.arn
     # forward {
     #   target_group {
     #     arn =     aws_lb_target_group.blue.arn
